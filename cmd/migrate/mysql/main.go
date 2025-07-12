32f422b0ee9fde0903ed841750cc2e444c2c9fe3
Maxim <teakeyx@gmail.com>
2025-07-12 16:42:57 +0300

    init migrations

---
+++ cmd/migrate/mysql/main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"database/sql"
+	"expire-share/internal/config"
+	pkgLog "expire-share/internal/lib/log"
+	"expire-share/internal/lib/log/sl"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/golang-migrate/migrate"
+	mysqlMigrate "github.com/golang-migrate/migrate/database/mysql"
+	_ "github.com/golang-migrate/migrate/source/file"
+	"log"
+	"os"
+)
+
+func main() {
+	migrateFunc := map[string]func(*migrate.Migrate) error{
+		"up": func(mgr *migrate.Migrate) error {
+			return mgr.Up()
+		},
+		"down": func(mgr *migrate.Migrate) error {
+			return mgr.Down()
+		},
+	}
+
+	cfg := config.MustLoad()
+	lg, err := pkgLog.New(cfg.Environment)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if len(os.Args) < 2 {
+		log.Fatal("usage: migrate [up/down]")
+	}
+
+	cmd := os.Args[1]
+
+	db, err := sql.Open("mysql", cfg.ConnectionString)
+	if err != nil {
+		lg.Error("failed to open database", sl.Error(err))
+		os.Exit(1)
+	}
+
+	defer func(db *sql.DB) {
+		err := db.Close()
+		if err != nil {
+			lg.Error("failed to close database", sl.Error(err))
+		}
+	}(db)
+
+	if err := db.Ping(); err != nil {
+		lg.Error("failed to ping database", sl.Error(err))
+		os.Exit(1)
+	}
+
+	driver, err := mysqlMigrate.WithInstance(db, &mysqlMigrate.Config{})
+	if err != nil {
+		lg.Error("failed to create database driver", sl.Error(err))
+		os.Exit(1)
+	}
+
+	mgr, err := migrate.NewWithDatabaseInstance(
+		"file://migrations",
+		"mysql",
+		driver)
+
+	if err != nil {
+		lg.Error("failed to create migration instance", sl.Error(err))
+		os.Exit(1)
+	}
+
+	fn, ok := migrateFunc[cmd]
+	if !ok {
+		lg.Error(fmt.Sprintf("unknown command: %s", cmd))
+		os.Exit(1)
+	}
+
+	if err := fn(mgr); err != nil {
+		lg.Error("failed to migrate table", sl.Error(err))
+		os.Exit(1)
+	}
+
+	lg.Info(fmt.Sprintf("migrate %s finished successfully", cmd))
+}
--- internal/storage/mysql/mysql.go
+++ internal/storage/mysql/mysql.go
@@ -4,9 +4,6 @@ import (
 	"database/sql"
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/golang-migrate/migrate"
-	mysqlMigrate "github.com/golang-migrate/migrate/database/mysql"
-	_ "github.com/golang-migrate/migrate/source/file"
 )
 
 type Storage struct {
@@ -23,24 +20,6 @@ func New(connectionString string) (*Storage, error) {
 
 	if err := db.Ping(); err != nil {
 		return nil, fmt.Errorf("%s: failed to ping database: %v", fn, err)
-	}
-
-	driver, err := mysqlMigrate.WithInstance(db, &mysqlMigrate.Config{})
-	if err != nil {
-		return nil, fmt.Errorf("%s: failed to create database driver: %v", fn, err)
-	}
-
-	mgr, err := migrate.NewWithDatabaseInstance(
-		"file://migrations",
-		"mysql",
-		driver)
-
-	if err != nil {
-		return nil, fmt.Errorf("%s: failed to create migration instance: %v", fn, err)
-	}
-
-	if err := mgr.Up(); err != nil {
-		return nil, fmt.Errorf("%s: failed to run migrations: %v", fn, err)
 	}
 
 	return &Storage{Database: db}, nil
