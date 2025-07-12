bdd139c7da9ccc35c6eb1e1feb74a12fa28c3084
Maxim <teakeyx@gmail.com>
2025-07-13 01:19:23 +0300

    init chi router:

--- cmd/expire-share/main.go
+++ cmd/expire-share/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"errors"
 	"expire-share/internal/config"
 	pkgLog "expire-share/internal/lib/log"
 	"expire-share/internal/lib/log/sl"
+	myMiddleware "expire-share/internal/middlewares"
 	"expire-share/internal/storage/mysql"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 	"log"
 	"log/slog"
+	"net/http"
 	"os"
 )
 
@@ -18,7 +23,7 @@ func main() {
 		log.Fatal("failed to initialize logger:", err)
 	}
 
-	lg.Info("starting expire share server", slog.String("environment", cfg.Environment))
+	lg.Info("starting expire share", slog.String("environment", cfg.Environment))
 
 	storage, err := mysql.New(cfg.ConnectionString)
 	if err != nil {
@@ -32,4 +37,30 @@ func main() {
 			lg.Error("failed to close storage:", sl.Error(err))
 		}
 	}()
+
+	lg.Info("storage was initialized successfully", slog.String("connection_string", cfg.ConnectionString))
+
+	router := chi.NewRouter()
+
+	router.Use(middleware.RequestID)
+	router.Use(middleware.RealIP)
+	router.Use(middleware.Recoverer)
+	router.Use(middleware.URLFormat)
+	router.Use(myMiddleware.NewLogger(lg))
+
+	lg.Info("starting expire share server", slog.String("address", cfg.HttpServer.Address))
+
+	server := http.Server{
+		Addr:         cfg.HttpServer.Address,
+		Handler:      router,
+		ReadTimeout:  cfg.Timeout,
+		WriteTimeout: cfg.Timeout,
+		IdleTimeout:  cfg.IdleTimeout,
+	}
+
+	if err := server.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+		lg.Error("failed to start http server:", sl.Error(err))
+	}
+
+	lg.Error("server stopped")
 }
--- go.mod
+++ go.mod
@@ -3,9 +3,10 @@ module expire-share
 go 1.23.2
 
 require (
+	github.com/go-chi/chi v1.5.5
+	github.com/go-chi/chi/v5 v5.2.2
 	github.com/go-sql-driver/mysql v1.9.3
 	github.com/golang-migrate/migrate v3.5.4+incompatible
-	github.com/golang-migrate/migrate/v4 v4.18.3
 	github.com/ilyakaznacheev/cleanenv v1.5.0
 	github.com/joho/godotenv v1.5.1
 )
@@ -13,9 +14,20 @@ require (
 require (
 	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/BurntSushi/toml v1.2.1 // indirect
+	github.com/Microsoft/go-winio v0.6.2 // indirect
 	github.com/containerd/errdefs v1.0.0 // indirect
 	github.com/containerd/errdefs/pkg v0.3.0 // indirect
+	github.com/distribution/reference v0.6.0 // indirect
 	github.com/docker/docker v28.3.2+incompatible // indirect
+	github.com/docker/go-connections v0.5.0 // indirect
+	github.com/docker/go-units v0.5.0 // indirect
+	github.com/moby/docker-image-spec v1.3.1 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/opencontainers/image-spec v1.1.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.54.0 // indirect
+	go.opentelemetry.io/otel/trace v1.29.0 // indirect
+	golang.org/x/sys v0.31.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
 )
