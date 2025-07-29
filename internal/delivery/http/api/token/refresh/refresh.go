c9454a40b4e7225dd70d762692ef164c33b0f9b1
Maxim <teakeyx@gmail.com>
2025-07-29 23:22:26 +0300

    add omitempty tag to response

--- internal/delivery/http/api/token/refresh/refresh.go
+++ internal/delivery/http/api/token/refresh/refresh.go
@@ -18,8 +18,8 @@ type Request struct {
 
 type Response struct {
 	response.Response
-	AccessToken  string `json:"access_token"`
-	RefreshToken string `json:"refresh_token"`
+	AccessToken  string `json:"access_token,omitempty"`
+	RefreshToken string `json:"refresh_token,omitempty"`
 }
 
 type TokenRefresher interface {
