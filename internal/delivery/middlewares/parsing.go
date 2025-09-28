4b1699fb91f711205da60c8a554fdae7f5b9764e
Maxim <teakeyx@gmail.com>
2025-09-28 23:01:00 +0300

    improved BodyParser middleware

--- internal/delivery/middlewares/parsing.go
+++ internal/delivery/middlewares/parsing.go
@@ -5,29 +5,49 @@ import (
 	"expire-share/internal/lib/api/response"
 	"expire-share/internal/lib/log/sl"
 	"github.com/go-chi/render"
-	"io"
 	"log/slog"
 	"net/http"
 )
 
-const fieldName = "request"
+const (
+	fieldName = "request"
+	maxBytes  = 10 * 1024 * 1024
+)
 
-type BodyParserSettings struct {
-	BodyIsOptional bool
-}
-
-func NewBodyParser(log *slog.Logger, settings BodyParserSettings) func(http.Handler) http.Handler {
+func NewBodyParser(log *slog.Logger) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		log = log.With(slog.String("component", "middleware/parsing"))
 
+		methodsToSkip := map[string]bool{
+			http.MethodGet:     true,
+			http.MethodHead:    true,
+			http.MethodDelete:  true,
+			http.MethodOptions: true,
+		}
+
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			var request interface{}
-			err := render.DecodeJSON(r.Body, &request)
-			if err != nil && (err == io.EOF && !settings.BodyIsOptional) {
-				log.Info("failed to decode json body", sl.Error(err))
+			r.Body = http.MaxBytesReader(w, r.Body, maxBytes)
+
+			if methodsToSkip[r.Method] {
+				next.ServeHTTP(w, r)
+				return
+			}
+
+			if r.ContentLength <= 0 {
+				log.Info("request body is empty")
 				response.RenderError(w, r,
 					http.StatusBadRequest,
-					"failed to decode json body")
+					"request body is empty")
+				return
+			}
+
+			var request interface{}
+			err := render.DecodeJSON(r.Body, &request)
+			if err != nil {
+				log.Info("failed to decode request body", sl.Error(err))
+				response.RenderError(w, r,
+					http.StatusBadRequest,
+					"failed to decode request body")
 				return
 			}
 
