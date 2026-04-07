d5c386c0c60cdc18427ae9d20a0232a8f372d0c0
Maxim <teakeyx@gmail.com>
2026-04-07 21:06:28 +0300

    fix: add storage to file worker

--- internal/services/worker/worker.go
+++ internal/services/worker/worker.go
@@ -4,11 +4,10 @@ import (
 	"context"
 	"expire-share/internal/config"
 	"expire-share/internal/domain/interfaces/repositories"
+	"expire-share/internal/domain/interfaces/storage"
 	"expire-share/internal/lib/log/sl"
 	"fmt"
 	"log/slog"
-	"os"
-	"path/filepath"
 	"time"
 )
 
@@ -16,12 +15,13 @@ type FileWorker struct {
 	Delay time.Duration
 	cfg   config.Config
 	repo  repositories.FileRepo
+	files storage.File
 	log   *slog.Logger
 }
 
 func (fw *FileWorker) Start(ctx context.Context) {
 	const fn = "services.worker.FileWorker.Start"
-	fw.log = slog.With(slog.String("fn", fn))
+	log := fw.log.With(slog.String("fn", fn))
 
 	ticker := time.NewTicker(fw.Delay)
 	defer ticker.Stop()
@@ -29,32 +29,37 @@ func (fw *FileWorker) Start(ctx context.Context) {
 	for {
 		select {
 		case <-ctx.Done():
-			fw.log.Info("file worker stopped")
+			log.Info("file worker stopped")
 			return
 
 		case <-ticker.C:
 			aliases, err := fw.repo.DeleteExpiredFiles(ctx)
 			if err != nil {
-				fw.log.Error("failed to delete expired files", sl.Error(err))
+				log.Error("failed to delete expired files", sl.Error(err))
 			}
 
 			for _, alias := range aliases {
-				folderPath := filepath.Join(fw.cfg.Path, alias)
-				if err := os.RemoveAll(folderPath); err != nil {
-					fw.log.Error("failed to delete file from storage", sl.Error(err))
+				if err := fw.files.Delete(alias); err != nil {
+					log.Warn("failed to delete file from storage", sl.Error(err))
 				}
 			}
 
-			fw.log.Info(fmt.Sprintf("deleted %d expired files", len(aliases)))
+			if len(aliases) > 0 {
+				log.Info(fmt.Sprintf("deleted %d expired files", len(aliases)))
+				continue
+			}
+
+			log.Debug("deleted 0 expired files")
 		}
 	}
 }
 
-func NewFileWorker(repo repositories.FileRepo, log *slog.Logger, cfg config.Config) *FileWorker {
+func NewFileWorker(repo repositories.FileRepo, files storage.File, log *slog.Logger, cfg config.Config) *FileWorker {
 	return &FileWorker{
 		Delay: cfg.FileWorkerDelay,
 		cfg:   cfg,
 		repo:  repo,
+		files: files,
 		log:   log,
 	}
 }
