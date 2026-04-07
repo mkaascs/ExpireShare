8674acaf06b3b76e7ca7a59e0bb2885af2e7b85d
Maxim <teakeyx@gmail.com>
2026-04-07 22:12:45 +0300

    refactor: added tx in file worker

--- internal/services/worker/worker.go
+++ internal/services/worker/worker.go
@@ -2,22 +2,23 @@ package worker
 
 import (
 	"context"
+	"errors"
 	"expire-share/internal/config"
 	"expire-share/internal/domain/interfaces/repositories"
 	"expire-share/internal/domain/interfaces/storage"
 	"expire-share/internal/lib/log/sl"
-	"fmt"
 	"log/slog"
 	"time"
 )
 
 type FileWorker struct {
 	Delay time.Duration
-	cfg   config.Config
 	repo  repositories.FileRepo
 	files storage.File
 	log   *slog.Logger
 }
+
+const batchLimit = 100
 
 func (fw *FileWorker) Start(ctx context.Context) {
 	const fn = "services.worker.FileWorker.Start"
@@ -29,23 +30,57 @@ func (fw *FileWorker) Start(ctx context.Context) {
 	for {
 		select {
 		case <-ctx.Done():
-			log.Info("file worker stopped")
+			log.Info("file worker stopping")
 			return
 
 		case <-ticker.C:
-			aliases, err := fw.repo.DeleteExpiredFiles(ctx)
+			tx, err := fw.repo.BeginTx(ctx)
 			if err != nil {
-				log.Error("failed to delete expired files", sl.Error(err))
+				log.Warn("failed to begin tx. trying again in 5s", sl.Error(err))
+				time.Sleep(5 * time.Second)
+				continue
 			}
 
-			for _, alias := range aliases {
-				if err := fw.files.Delete(alias); err != nil {
-					log.Warn("failed to delete file from storage", sl.Error(err))
+			rollback := func() {
+				if err := tx.Rollback(); err != nil {
+					log.Warn("failed to rollback tx", sl.Error(err))
 				}
 			}
 
+			aliases, err := fw.repo.DeleteExpiredFilesTx(ctx, tx, batchLimit)
+			if err != nil {
+				log.Warn("failed to delete expired files from repo", sl.Error(err))
+				rollback()
+				continue
+			}
+
+			success := true
+			for _, alias := range aliases {
+				if err := fw.files.Delete(ctx, alias); err != nil {
+					if errors.Is(err, context.Canceled) {
+						log.Info("file worker stopping", slog.String("alias", alias))
+						rollback()
+						return
+					}
+
+					log.Warn("failed to delete file from storage", sl.Error(err), slog.String("alias", alias))
+					success = false
+					break
+				}
+			}
+
+			if !success {
+				rollback()
+				continue
+			}
+
+			if err := tx.Commit(); err != nil {
+				log.Warn("failed to commit tx", sl.Error(err))
+				continue
+			}
+
 			if len(aliases) > 0 {
-				log.Info(fmt.Sprintf("deleted %d expired files", len(aliases)))
+				log.Info("deleted expired files", slog.Int("count", len(aliases)))
 				continue
 			}
 
@@ -57,7 +92,6 @@ func (fw *FileWorker) Start(ctx context.Context) {
 func NewFileWorker(repo repositories.FileRepo, files storage.File, log *slog.Logger, cfg config.Config) *FileWorker {
 	return &FileWorker{
 		Delay: cfg.FileWorkerDelay,
-		cfg:   cfg,
 		repo:  repo,
 		files: files,
 		log:   log,
